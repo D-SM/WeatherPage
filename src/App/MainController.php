2ef03373fbfd388617257799b34d31c87a8140e8
user <user@DESKTOP-CVL9BA2>
2016-10-27 20:40:34 +0200

    new class

--- src/App/MainController.php
+++ src/App/MainController.php
@@ -9,15 +9,19 @@
 namespace App;
 
 class MainController extends AbstractController {
-    public function renderPage()
+    public function renderPage($id)
     {
         $apiModel = new \WeatherAPI\Model\Current();
-        
+        $citiesModel = new \Cities($id);
+        $userCities[] = $citiesModel;
         // @todo odczytanie jakie miasta sa w profilu
         
         $cities = [];
         
         //@todo foreach po odczytanych miastach
+        foreach (city as $userCities){
+            var_dump($cities[''])
+        };
         $cities[] = $apiModel->getWeather('warsaw');
         $cities[] = $apiModel->getWeather('berlin');
 
