6cca1401d53a4b839165e5af3acc84a921ef6329
Jacek <jacek.czubala@gmail.com>
2016-10-29 09:14:35 +0200

    changes

--- src/App/MainController.php
+++ src/App/MainController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: mateusz
@@ -9,24 +10,21 @@
 namespace App;
 
 class MainController extends AbstractController {
-    public function renderPage($id)
-    {
-        $apiModel = new \WeatherAPI\Model\Current();
-        $citiesModel = new \Cities($id);
-        $userCities[] = $citiesModel;
-        // @todo odczytanie jakie miasta sa w profilu
-        
-        $cities = [];
-        
-        //@todo foreach po odczytanych miastach
-        foreach (city as $userCities){
-            var_dump($cities[''])
-        };
-        $cities[] = $apiModel->getWeather('warsaw');
-        $cities[] = $apiModel->getWeather('berlin');
 
-        return $this->twig->render('main-page.twig', [
-            'cities' => $cities
+    public function renderPage() {
+        $apiModel = new \WeatherAPI\Model\Current();
+        // @todo odczytanie jakie miasta sa w profilu
+
+        $cities = [];
+
+        //@todo foreach po odczytanych miastach
+        
+            $cities[] = $apiModel->getWeather('warsaw');
+            $cities[] = $apiModel->getWeather('berlin');
+
+            return $this->twig->render('main-page.twig', [
+                        'cities' => $cities
             ]);
-    }
+        }
+    
 }
